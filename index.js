0f95aea6e335f2acf3f92b887df76fb7866c5ea7
beam2711 <s6606022610064@email.kmutnb.ac.th>
2026-01-14 10:45:45 +0700

    wowwwww

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 
-const express = Request("express");
+const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
